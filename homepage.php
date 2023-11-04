724d4973d97c2c9055c06f717677753b8d076f31
sri-vaish-navi <148298779+sri-vaish-navi@users.noreply.github.com>
2023-11-05 01:21:08 +0800

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link rel="stylesheet" href="asset\style\navbar_footer.css">
+</head>
+<body>
+    <div class="navbar">
+        <div class="navbar-logo">
+            <img src="asset\img\logo.png" alt="Logo">
+        </div>
+        <div class="navbar-links">
+            <a href="homepage.php">Home</a>
+            <a href="#">Promotions</a>
+            <a href="gift.php">Gifts & Vouchers</a>
+        </div>
+    </div>
+
+    <div class="div-body">
+    </div>
+
+    <section class="mt-5">
+        <h5 class="title-header">Now Showing: </p>
+        <br>
+        <div class="container">
+            <h6 class="info-movies">Avatar: The Way of Water</h6>
+            <img src="asset\img\avatar.png" width="200" alt="Avatar">
+            <p class="info-movies">Duration:</p>
+            <p class="info-movies">Rating:</p>
+            <p class="info-movies">Description:</p>
+             
+        </div>
+
+    </section>
+        <?php
+            //for php codes
+        ?>
+    </div>
+
+    <section class="mt-5">
+        <h5 class="title-header">Advance Sales:</p>
+        <br>
+        <div class="container">
+            <h6 class="info-movies">Talk to me</h6>
+            <img src="asset\img\talktome.png" width="200" alt="Avatar">
+            <p class="info-movies">Duration:</p>
+            <p class="info-movies">Rating:</p>
+            <p class="info-movies">Description:</p>
+             
+        </div>
+
+    </section>
+     
+    <div class="div-body">
+    </div>
+
+
+    <footer>
+        <div class="footer-row">
+            <p>Supported Payment</p>
+        </div>
+        <div class="footer-row">
+            <img src="asset\img\visa-mastercard-logos.jpg" alt="Supported Payment">
+        </div>
+        <div class="footer-row">
+            <p>&copy; 2023 Diamond Bay</p>
+        </div>
+    </footer>
+    
+</body>
+</html>
