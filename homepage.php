345ebe3ffa46448368711c2346f54904cac1acc9
sri-vaish-navi <148298779+sri-vaish-navi@users.noreply.github.com>
2023-11-05 04:01:21 +0800

    Updated homepage.php

--- homepage.php
+++ homepage.php
@@ -20,7 +20,9 @@
     <div class="div-body">
     </div>
 
-    <section class="mt-5">
+    
+
+    <!-- <section class="mt-5">
         <h5 class="title-header">Now Showing: </p>
         <br>
         <div class="container">
@@ -32,13 +34,47 @@
              
         </div>
 
-    </section>
-        <?php
-            //for php codes
-        ?>
-    </div>
+    </section> -->
 
-    <section class="mt-5">
+    <?php
+        // Include your database connection code
+        include('db_connection.php');
+
+
+        // Query the database to select the data from the 'movie' table.
+        $query = "SELECT * FROM movie WHERE tag = 'Now Showing' ";
+        $result = mysqli_query($connection, $query);
+
+        if (!$result) {
+            die("Query failed: " . mysqli_error($connection));
+        }
+    
+        echo '<h2 class="title-header">Now Showing</h2>';
+        echo '<br>';
+
+        echo '<div class="container custom-right-shift">';
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<div class="card">';
+            echo '<h3>' . $row['name'] . '</h3>';
+            echo '<a href="movie_detail.php?id=' . $row['id'] . '">';
+            echo '<div class="image-container" style="max-width: 200px;">
+            <img src="asset/img/' . $row['movie_banner'] . '" alt="' . $row['movie_banner'] . '" style="width: 100%;"></a>
+            <br>
+            <p class="movie-desc2">' . $row['duration'] . '</p>
+            <p class="movie-desc2">' . $row['rating'] . '</p>
+            <br>
+            <p class="movie-desc">' . $row['description'] . '</p>
+        </div>';
+            echo '<br>';
+        }
+        echo '</div>';
+    ?>
+    
+
+    
+
+
+    <!-- <section class="mt-5">
         <h5 class="title-header">Advance Sales:</p>
         <br>
         <div class="container">
@@ -50,8 +86,44 @@
              
         </div>
 
-    </section>
-     
+    </section> -->
+
+
+    <?php
+        // Include your database connection code
+        include('db_connection.php');
+
+
+        // Query the database to select the data from the 'movie' table.
+        $query = "SELECT * FROM movie WHERE tag = 'Advance Sales' ";
+        $result = mysqli_query($connection, $query);
+
+        if (!$result) {
+            die("Query failed: " . mysqli_error($connection));
+        }
+    
+        echo '<br>';
+        echo '<br>';
+        echo '<h2>Advance Sales</h2>';
+        echo '<br>';
+
+        echo '<div class="container custom-right-shift">';
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<div>';
+            echo '<h3>' . $row['name'] . '</h3>';
+            echo '<a href="movie_detail.php?id=' . $row['id'] . '">';
+            echo '<div style="max-width: 200px;">
+            <img src="asset/img/' . $row['movie_banner'] . '" alt="' . $row['movie_banner'] . '" style="width: 100%;"></a>
+            <br>
+            <p>' . $row['duration'] . '</p>
+            <p>' . $row['rating'] . '</p>
+            <br>
+            <p>' . $row['description'] . '</p>
+        </div>';
+            echo '<br>';
+        }
+    ?> 
+  
     <div class="div-body">
     </div>
 
@@ -67,6 +139,7 @@
             <p>&copy; 2023 Diamond Bay</p>
         </div>
     </footer>
-    
+
+
 </body>
 </html>
