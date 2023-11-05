6b72b1826b2f0ed8500fdc447e12bd213fe698fe
sri-vaish-navi <148298779+sri-vaish-navi@users.noreply.github.com>
2023-11-05 17:22:04 +0800

    movie details page

---
+++ movie_detail.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link rel="stylesheet" href="asset\style\movies.css">
+</head>
+<body>
+    <div class="navbar">
+        <div class="navbar-logo">
+            <img src="asset\img\logo.png" alt="Logo">
+        </div>
+        <div class="navbar-links">
+            <a href="homepage.php">Home</a>
+            <a href="#">Promotions</a>
+            <a href="gift.php">Gifts & Vouchers</a>
+        </div>
+    </div>
+
+    <div class="div-body">
+    </div>
+
+    <footer>
+        <div class="footer-row">
+            <p>Supported Payment</p>
+        </div>
+        <div class="footer-row">
+            <img src="asset\img\visa-mastercard-logos.jpg" alt="Supported Payment">
+        </div>
+        <div class="footer-row">
+            <p>&copy; 2023 Diamond Bay</p>
+        </div>
+    </footer>
+
+
+</body>
+</html>
