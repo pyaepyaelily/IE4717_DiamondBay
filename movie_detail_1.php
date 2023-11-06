b4b56cbabc1a18acccb9cb0911de2a0b62fb6a7b
pyaepyaelily <pyaepyaelily@gmail.com>
2023-11-06 18:37:07 +0800

    added session

--- movie_detail_1.php
+++ movie_detail_1.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home</title>
     <link rel="stylesheet" href="asset\style\movie_details.css">
+    <link rel="stylesheet" href="asset\style\navbar_footer.css">
     <script src="movie_date.js"></script> <!-- Link to the external script -->
 </head>
 
@@ -22,7 +23,8 @@
 
     <div class="div-body">
         <?php
-        // Include your database connection code
+        session_start();
+
         include('db_connection.php');
 
         // Get the promotion ID from the URL
@@ -37,62 +39,79 @@
         }
 
         if ($row = mysqli_fetch_assoc($result)) {
-            echo '<h1 class="title-header">' . $row['name'] . '</h1>';
+
+            $movieName = $row['name'];
+            $movieImg =  $row['movie_banner'];
+            $movieRating = $row['rating'];
+            $movieDuration = $row['duration'];
+
+            // Store the movie name in a session variable
+            $_SESSION['movieName'] = $movieName;
+            $_SESSION['movieImg'] = $movieImg;
+            $_SESSION['movieRating'] = $movieRating;
+            $_SESSION['movieDuration'] = $movieDuration;
+
+            echo '<ul class="breadcrumb">';
+            echo '<li><a href="#">Home</a></li>';
+            echo "<li>$movieName</li>";
+            echo '</ul>';
+
+            echo "<h1 class='title-header'>$movieName</h1>";
             echo '<div class="movie-details-container">';
 
             echo '<div class="banner-container">';
-            echo '<img src="asset/img/' . $row['movie_banner'] . '" alt="' . $row['movie_banner'] . '">';
+            echo "<img src='asset/img/$movieImg ' alt='$movieImg'>";
             echo '</div>';
 
             echo '<div class="description-container">';
             echo '<p class="movie-description">' . $row['description'] . '</p>';
             echo '<div><br>';
 
-            echo '<p class="movie-dur">' . $row['duration'] . '</p><br>';
-            echo '<p class="movie-rate">' . $row['rating'] . '</p>';
+            echo "<p class='movie-dur'> $movieDuration</p>";
             echo '<br>';
-
-            // echo '</div>';
-            // echo '</div>';
-            // echo '</div>';
+            echo "<p class='movie-rate'> $movieRating</p>";
+            echo '<br>';
             echo '<br>';
         } else {
             echo '<p class="movie-details-error">Movie not found</p>';
         }
 
 
-
-        $show_time_query = "SELECT DISTINCT theatre, DATE(start_time) AS show_date FROM show_time WHERE movie_id = $movieId ORDER BY theatre ASC, show_date ASC;";
+        $show_time_query = "SELECT DISTINCT theatre, movie_date FROM show_time WHERE movie_id = $movieId ORDER BY theatre ASC, movie_date ASC, movie_time ASC;";
         $show_time_result = mysqli_query($connection, $show_time_query);
-
+        
         while ($show_time_row = mysqli_fetch_assoc($show_time_result)) {
             $theater = $show_time_row['theatre'];
-            $show_date = $show_time_row['show_date'];
+            $movie_date = $show_time_row['movie_date'];
+        
             echo '<p class="theater-button">' . $theater . '</p>';
-
             // Add a hidden div for each theater and show date to display start times
-            echo '<div class="start-times" data-theater="' . $theater . '" data-show-date="' . $show_date . '">';
-            $start_times_query = "SELECT * FROM show_time WHERE movie_id = $movieId AND theatre = '$theater' AND DATE(start_time) = '$show_date' ORDER BY start_time ASC;";
-            $start_times_result = mysqli_query($connection, $start_times_query);
-
-            while ($start_time_row = mysqli_fetch_assoc($start_times_result)) {
-                $start_time = date('His', strtotime($start_time_row['start_time'])); // Extract the time portion without colons
-                $start_time = substr($start_time, 0, 4); // Get the first four characters (HHMM)
-                $hallNumber = $start_time_row['hall'];
-                $show_time_ID = $start_time_row['id'];
-
+            echo '<div class="start-times" data-theater="' . $theater . '" data-show-date="' . $movie_date . '">';
+        
+            $show_dateTime_query = "SELECT id, movie_time, hall FROM show_time WHERE movie_id = $movieId AND theatre = '$theater' AND movie_date = '$movie_date' ORDER BY movie_time ASC;";
+            $show_dateTime_result = mysqli_query($connection, $show_dateTime_query);
+        
+            while ($show_dateTime_row = mysqli_fetch_assoc($show_dateTime_result)) {
+                $movie_time = substr($show_dateTime_row['movie_time'], 0, 5); // Extract only HH:MM
+                $hallNumber = $show_dateTime_row['hall'];
+                $show_time_ID = $show_dateTime_row['id']; // Define $show_time_ID
+        
+                $_SESSION['theater'] = $theater;
+                $_SESSION['movie_date'] = $movie_date;
+                $_SESSION['movie_time'] = $movie_time;
+                $_SESSION['hallNumber'] = $hallNumber;
+        
                 // Create a button with an embedded anchor tag using the desired URL format
-                $url = "seat_selection_1.php?id=" .urlencode($show_time_ID) . "&showDate=" . urlencode(date('d-m-Y', strtotime($show_date))) . "&showTime=" . urlencode($start_time) . "&hallNumber=" . urlencode($hallNumber);
-                echo '<button class="start-time-button"><a href="' . $url . '">' . $start_time . '</a></button>';
+                $url = "seat_selection_1.php?id=" . urlencode($show_time_ID) . "&showDate=" . urlencode($movie_date) . "&showTime=" . urlencode($movie_time) . "&hallNumber=" . urlencode($hallNumber);
+                echo '<button class="start-time-button"><a href="' . $url . '">' . $movie_time . '</a></button>';
             }
-
+        
             echo '</div>';
-
+        
             // Add a <br> element to create space after each theater section
             echo '<br>';
         }
-
-
+        
 
 
         echo '</div>';
